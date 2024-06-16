ce8d37a9b312f58bc0239a4cca44115df23e9d0b
Wilson Neto <contato@wilsonneto.com.br>
2024-06-16 20:29:09 -0300

    HttpClient instantiated in the gateway, first example

--- WebApplication1/OpenAi/OpenAiGateway.cs
+++ WebApplication1/OpenAi/OpenAiGateway.cs
@@ -1,4 +1,5 @@
 using Microsoft.Extensions.Options;
+using WebApplication1.OpenAi.Models;
 using WebApplication1.OpenAi.Settings;
 
 namespace WebApplication1.OpenAi;
@@ -8,6 +9,16 @@ internal class OpenAiGateway(IOptions<OpenAiSettings> openAiSettings)
     private readonly string _baseAddress = openAiSettings.Value.BaseAddress;
     private readonly string _apiKey = openAiSettings.Value.ApiKey;
     
-    public Task<string> ExecutePrompt(string prompt) => 
-        Task.FromResult($"BaseAddress: {_baseAddress} / Api Key: {_apiKey}");
+    public async Task<string> ExecutePrompt(string prompt)
+    {
+        var client = new HttpClient { BaseAddress = new (_baseAddress) };
+        client.DefaultRequestHeaders.Add("Authorization", $"Bearer {_apiKey}");
+
+        var httpResponse = await client.PostAsJsonAsync("chat/completions", 
+            new CompletionsRequest("gpt-4o", [ new Message("user", prompt) ]));
+
+        var response = await httpResponse.Content.ReadFromJsonAsync<CompletionsResponse>();
+        
+        return response!.Choices[0].Message.Content;
+    }
 }
